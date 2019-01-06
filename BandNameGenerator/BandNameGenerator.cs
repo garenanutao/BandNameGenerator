60d12886300d9f2768502c3bce5850b5e424ea84
Sheng Fu,Ke <w91090@gmail.com>
2019-01-06 18:34:54 +0800

    first commit

---
+++ BandNameGenerator/BandNameGenerator.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BandNameGenerator
+{
+    public class Kata
+    {
+        public static string BandNameGenerator(string str)
+        {
+            if (str == null) return "n/a";
+            char firstChar = (char)((int)str[0] - 32);
+            string restOfStr = str.Substring(1, str.Length - 1);
+            if (str[0] != str[str.Length - 1]) return ("The " + firstChar + restOfStr);
+            else return (firstChar + restOfStr + restOfStr);
+        }
+    }
+}
---
+++ BandNameGeneratorTests/KataTests.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BandNameGenerator;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BandNameGenerator.SolutionTest
+{
+    [TestClass()]
+    public class KataTests
+    {
+        [TestMethod()]
+        public void BandNameGeneratorSampleTest()
+        {
+            Assert.AreEqual("The Knife", Kata.BandNameGenerator("knife"));
+            Assert.AreEqual("Tartart", Kata.BandNameGenerator("tart"));
+            Assert.AreEqual("Sandlesandles", Kata.BandNameGenerator("sandles"));
+            Assert.AreEqual("The Bed", Kata.BandNameGenerator("bed"));
+        }
+    }
+}
